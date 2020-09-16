5168fed669f7a438e03160671f84a677a6516541
Yurii Oleshko <yurii.oleshko@gmail.com>
2020-09-16 09:27:54 +0300

    Updates

---
+++ precache-manifest.f48a24fe4eddfb094af60d225b2665f3.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc3b5f798d6d9f87a2f01f3d539f2fe",
+    "url": "/game-test-2/index.html"
+  },
+  {
+    "revision": "5387f6b2abb1d5a4404a",
+    "url": "/game-test-2/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "24f19064585c79dfcb6b",
+    "url": "/game-test-2/static/css/main.87258bfe.chunk.css"
+  },
+  {
+    "revision": "5387f6b2abb1d5a4404a",
+    "url": "/game-test-2/static/js/2.9d4af2ed.chunk.js"
+  },
+  {
+    "revision": "d3eab2c800484648f446253871549a7a",
+    "url": "/game-test-2/static/js/2.9d4af2ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52cc767e0400b01fa515",
+    "url": "/game-test-2/static/js/3.71d793ae.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/game-test-2/static/js/3.71d793ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24f19064585c79dfcb6b",
+    "url": "/game-test-2/static/js/main.65089335.chunk.js"
+  },
+  {
+    "revision": "68ced3ab8f184142c0cd",
+    "url": "/game-test-2/static/js/runtime-main.a1d1fae9.js"
+  },
+  {
+    "revision": "6ce7556772b18a79becd48f322d57c2b",
+    "url": "/game-test-2/static/media/533354__snapper4298__intro-outro-sequence-90bpm.6ce75567.wav"
+  },
+  {
+    "revision": "04fb8b4492381dc950a87a7e7475f2b3",
+    "url": "/game-test-2/static/media/Reso Bold.04fb8b44.woff"
+  },
+  {
+    "revision": "d026555a0933421697d7864f67547b08",
+    "url": "/game-test-2/static/media/Reso Light.d026555a.woff"
+  },
+  {
+    "revision": "a65c356036cbce95b2fe1ae1df53922f",
+    "url": "/game-test-2/static/media/Reso Regular.a65c3560.woff"
+  },
+  {
+    "revision": "c6d8c103a57c9ebed7848a34b081a62e",
+    "url": "/game-test-2/static/media/Reso SemiBold.c6d8c103.woff"
+  },
+  {
+    "revision": "12756cc4f4ae44e2648aefaede488db9",
+    "url": "/game-test-2/static/media/Reso Thin.12756cc4.woff"
+  },
+  {
+    "revision": "51fab851d4f52f9c26a400f53ebab382",
+    "url": "/game-test-2/static/media/Roboto-Regular.51fab851.ttf"
+  },
+  {
+    "revision": "7447c32c9bd2cd51ae5730c77a4036eb",
+    "url": "/game-test-2/static/media/Roboto-Regular.7447c32c.woff2"
+  },
+  {
+    "revision": "be6f7baa1a5da06f201eeeed2ff0f554",
+    "url": "/game-test-2/static/media/Roboto-Regular.be6f7baa.eot"
+  },
+  {
+    "revision": "c58564e2c68fe953dec102e4e0ab9344",
+    "url": "/game-test-2/static/media/Roboto-Regular.c58564e2.woff"
+  },
+  {
+    "revision": "9af2776035037ed67a9ed6d8259b5708",
+    "url": "/game-test-2/static/media/TrebuchetMS-Bold.9af27760.eot"
+  },
+  {
+    "revision": "a671f1409f271a57fae059f5eeb3b4cd",
+    "url": "/game-test-2/static/media/TrebuchetMS-Bold.a671f140.woff2"
+  },
+  {
+    "revision": "d0925a8d49c12c1556bbe95d17143a7b",
+    "url": "/game-test-2/static/media/TrebuchetMS-Bold.d0925a8d.woff"
+  },
+  {
+    "revision": "fffaaa521eab41513c0d24277a938e89",
+    "url": "/game-test-2/static/media/TrebuchetMS-Bold.fffaaa52.ttf"
+  },
+  {
+    "revision": "3c2cf93f3ef115976f9f3115dc643c78",
+    "url": "/game-test-2/static/media/TrebuchetMS.3c2cf93f.woff"
+  },
+  {
+    "revision": "877f72e1d3db01548f1fef12eb5ace17",
+    "url": "/game-test-2/static/media/TrebuchetMS.877f72e1.woff2"
+  },
+  {
+    "revision": "8899a69dc5d72aa02e9a8f2bda65eeee",
+    "url": "/game-test-2/static/media/TrebuchetMS.8899a69d.eot"
+  },
+  {
+    "revision": "d15dc0d86a594930e64367f69cb41fe2",
+    "url": "/game-test-2/static/media/TrebuchetMS.d15dc0d8.ttf"
+  },
+  {
+    "revision": "66b62cf0d7b1440abd12bbe8f5d05f7f",
+    "url": "/game-test-2/static/media/bg-game.66b62cf0.png"
+  },
+  {
+    "revision": "2724d6c5b28b4ebca212230fa6ea3e77",
+    "url": "/game-test-2/static/media/volume.2724d6c5.svg"
+  }
+]);
